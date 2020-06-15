798db5c19259ed6d92f7f2bf5f7c3032092080a2
crazybobik <yar.rybak@gmail.com>
2020-06-15 23:15:27 +0300

    Додав третій тестовий скрипт з типами даних і функціями

---
+++ edx_test/second_script.R
@@ -0,0 +1,13 @@
+# Title     : square equation
+# Objective : TODO
+# Created by: cb
+# Created on: 28.05.20
+
+a <- 1
+b <- 1
+c <- -1
+
+x1 = (-b + sqrt(b^2 - 4*a*c))/(2*a)
+x2 = (-b - sqrt(b^2 - 4*a*c))/(2*a)
+print(x1)
+print(x2)
---
+++ edx_test/third_script.R
@@ -0,0 +1,36 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: cb
+# Created on: 15.06.20
+
+#типи даних
+
+library(dslabs)
+data(murders)
+
+#показує таблицю
+class(murders)
+
+#показує структуру даних, функція str()
+str(murders)
+
+#показує перші 6 рядків даних, функція head()
+head(murders)
+
+#за допомогою $ можна дивитися окремі стовбці
+murders$population
+murders$state
+
+#назви "стовбців" функція names()
+names(murders)
+
+#довжина вектора lenght()
+pop <- murders$population
+length(pop)
+length(murders$population)
+
+#функція class() показує тип вектора
+class(pop)
+class(murders$region)
+
+levels(murders$region)
